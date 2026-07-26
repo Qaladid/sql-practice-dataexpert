8d766a944d41fc31c50f86e8f30fbd8341014577
Qaladid <abdiaziz11410@gmail.com>
2026-07-27 02:05:36 +0300

    added the solution of the birth question

--- easy/001-month-with-highest-total-births/solution.sql
+++ easy/001-month-with-highest-total-births/solution.sql
@@ -1,3 +1,7 @@
 -- Month with the Highest Total Births
 -- TODO: paste your working query here once solved
-
+SELECT month, SUM(births) AS total_births
+FROM playground.us_birth_stats
+GROUP BY month
+ORDER BY total_births DESC
+LIMIT 1
