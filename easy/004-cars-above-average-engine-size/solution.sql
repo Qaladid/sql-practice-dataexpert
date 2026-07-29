42b11ea47f939e5f80a88ad25a1d169e13577660
Qaladid <abdiaziz11410@gmail.com>
2026-07-29 23:54:41 +0300

    added the result.md file

---
+++ easy/004-cars-above-average-engine-size/solution.sql
@@ -0,0 +1,7 @@
+SELECT brand_name, fuel_type, engine_size
+FROM playground.automobile
+WHERE engine_size > (
+    SELECT AVG(engine_size)
+    FROM playground.automobile
+)
+ORDER BY engine_size DESC, brand_name ASC
